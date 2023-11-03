b287589fd68ad33c75197a05a659aa333dfdd820
Dziadzior <patrykroszkowski2020@gmail.com>
2023-11-03 16:26:22 +0100

    Do dokończenia
    Problematyczne zdjęcia

---
+++ plusik.js
@@ -0,0 +1,22 @@
+// Pokazanie sociali
+const pokaSocialki = (wlacznikKarta, socialkiKarta) => {
+  const wlacz = document.getElementById(wlacznikKarta),
+    socialki = document.getElementById(socialkiKarta);
+
+  wlacz.addEventListener("click", () => {
+    // Kiedy na odwrót to dodamy animacje na dół
+    if (socialki.classList.contains("animation")) {
+      socialki.classList.add("dol-animacja");
+    }
+
+    // Usuwamy dol-animacje
+    setTimeout(() => {
+      socialki.classList.remove("dol-animacja");
+    }, 1000);
+
+    // Animacja do socialek
+    socialki.classList.toggle("animation");
+  });
+};
+
+pokaSocialki("karta-wlacznik", "karta-linki");
